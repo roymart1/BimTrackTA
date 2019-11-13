6ca1d585c7dc55b4ebedaa41663fe90274205006
Martin-Pierre Roy <mroy@zentelia.com>
2019-11-12 22:21:59 -0500

    Minute change

--- BimTrackTA/API/APIInvokerTest.cs
+++ BimTrackTA/API/APIInvokerTest.cs
@@ -7,6 +7,7 @@ namespace BimTrackTA.API
     {
         public void TestCall()
         {
+            // To be tested
             string szKey = "3d77a3ae21baa8f69021904db31b25d8103c98beac5df608a8fb96e2f10f6f62";
             // arrange
             RestClient client = new RestClient("https://api.bimtrackapp.co");
